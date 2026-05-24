2ab4b3fb15936a95500f487bcf82861cb92ac280
ETERNALFYY <84897252+ETERNALFYY@users.noreply.github.com>
2026-05-24 21:28:19 +0800

    Update __init__.py

--- components/kelon_ac/__init__.py
+++ components/kelon_ac/__init__.py
@@ -1,1 +1,11 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+
+kelon_ac_ns = cg.esphome_ns.namespace("kelon_ac")
+KelonAC = kelon_ac_ns.class_("KelonAC", cg.Component)
+
+CONFIG_SCHEMA = cv.Schema({})
+
+async def to_code(config):
+    cg.new_Pvariable(config[KelonAC])
 
