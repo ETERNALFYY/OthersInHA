626f35390412f3ce688e6353e767f41f5a1d355c
ETERNALFYY <84897252+ETERNALFYY@users.noreply.github.com>
2026-05-24 20:36:01 +0800

    Create climate.py

---
+++ esphome/components/kelon_ac/climate.py
@@ -0,0 +1,21 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import climate
+from esphome.const import CONF_ID
+
+kelon_ns = cg.esphome_ns.namespace("kelon_ac")
+KelonAC = kelon_ns.class_("KelonAC", climate.Climate, cg.Component)
+
+CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend({
+    cv.GenerateID(): cv.declare_id(KelonAC),
+    cv.Optional("current_temperature_sensor"): cv.use_id(cg.Sensor),
+})
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await climate.register_climate(var, config)
+    await cg.register_component(var, config)
+
+    if "current_temperature_sensor" in config:
+        sensor = await cg.get_variable(config["current_temperature_sensor"])
+        cg.add(var.set_current_temperature_sensor(sensor))
