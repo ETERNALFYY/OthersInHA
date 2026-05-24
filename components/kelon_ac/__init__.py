eb5a5cda4f8a601c72ce4632b95836b38682c945
ETERNALFYY <84897252+ETERNALFYY@users.noreply.github.com>
2026-05-24 21:35:11 +0800

    Update __init__.py

--- components/kelon_ac/__init__.py
+++ components/kelon_ac/__init__.py
@@ -1,16 +1,1 @@
-import esphome.codegen as cg
-import esphome.config_validation as cv
-from esphome.components import climate
-
-kelon_ac_ns = cg.esphome_ns.namespace("kelon_ac")
-KelonAC = kelon_ac_ns.class_("KelonAC", climate.Climate, cg.Component)
-
-CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(KelonAC),
-})
-
-async def to_code(config):
-    var = cg.new_Pvariable(config[KelonAC])
-    await climate.register_climate(var, config)
-    await cg.register_component(var, config)
-
+# Required for ESPhome external_components
